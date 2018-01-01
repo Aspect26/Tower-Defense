f47022b0ddfb8c0907c6d4522124fd7976acd1fc
Aspect26 <flimmelj@gmail.com>
2018-01-01 17:15:09 +0100

    Fix stage 2 levels, tiles and props to correspond with new resolution
    #6

--- src/assets/levels/stage2/ice-props.tsx
+++ src/assets/levels/stage2/ice-props.tsx
@@ -2,57 +2,57 @@
 <tileset name="ice-props" tilewidth="40" tileheight="60" tilecount="18" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="40" height="40" source="props/Rune_Virgo.png"/>
+  <image width="40" height="40" source="Rune_Virgo.png"/>
  </tile>
  <tile id="1">
-  <image width="40" height="40" source="props/Rune_Water.png"/>
+  <image width="40" height="40" source="Rune_Water.png"/>
  </tile>
  <tile id="2">
-  <image width="40" height="40" source="props/Rune_Aquarius.png"/>
+  <image width="40" height="40" source="Rune_Aquarius.png"/>
  </tile>
  <tile id="3">
-  <image width="40" height="40" source="props/Rune_Aries.png"/>
+  <image width="40" height="40" source="Rune_Aries.png"/>
  </tile>
  <tile id="4">
-  <image width="40" height="40" source="props/Rune_blank.png"/>
+  <image width="40" height="40" source="Rune_blank.png"/>
  </tile>
  <tile id="5">
-  <image width="40" height="40" source="props/Rune_Cancer.png"/>
+  <image width="40" height="40" source="Rune_Cancer.png"/>
  </tile>
  <tile id="6">
-  <image width="40" height="40" source="props/Rune_capricorn.png"/>
+  <image width="40" height="40" source="Rune_capricorn.png"/>
  </tile>
  <tile id="7">
-  <image width="40" height="40" source="props/Rune_Fire.png"/>
+  <image width="40" height="40" source="Rune_Fire.png"/>
  </tile>
  <tile id="8">
-  <image width="40" height="40" source="props/Rune_Gemini.png"/>
+  <image width="40" height="40" source="Rune_Gemini.png"/>
  </tile>
  <tile id="9">
-  <image width="40" height="40" source="props/Rune_Ice.png"/>
+  <image width="40" height="40" source="Rune_Ice.png"/>
  </tile>
  <tile id="10">
-  <image width="40" height="40" source="props/Rune_Leo.png"/>
+  <image width="40" height="40" source="Rune_Leo.png"/>
  </tile>
  <tile id="11">
-  <image width="40" height="40" source="props/Rune_Libra.png"/>
+  <image width="40" height="40" source="Rune_Libra.png"/>
  </tile>
  <tile id="12">
-  <image width="40" height="40" source="props/Rune_Pisces.png"/>
+  <image width="40" height="40" source="Rune_Pisces.png"/>
  </tile>
  <tile id="13">
-  <image width="40" height="40" source="props/Rune_Sagittarius.png"/>
+  <image width="40" height="40" source="Rune_Sagittarius.png"/>
  </tile>
  <tile id="14">
-  <image width="40" height="40" source="props/Rune_Scorpio.png"/>
+  <image width="40" height="40" source="Rune_Scorpio.png"/>
  </tile>
  <tile id="15">
-  <image width="40" height="40" source="props/Rune_Taurus.png"/>
+  <image width="40" height="40" source="Rune_Taurus.png"/>
  </tile>
  <tile id="16">
-  <image width="40" height="40" source="props/bushSnowy.png"/>
+  <image width="40" height="40" source="bushSnowy.png"/>
  </tile>
  <tile id="17">
-  <image width="40" height="60" source="props/treeSnowy.png"/>
+  <image width="40" height="60" source="treeSnowy.png"/>
  </tile>
 </tileset>
--- src/assets/levels/stage2/ice-tileset.tsx
+++ src/assets/levels/stage2/ice-tileset.tsx
@@ -2,45 +2,45 @@
 <tileset name="ice-tileset" tilewidth="20" tileheight="20" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="20" height="20" source="tiles/stage_two/tile_01.png"/>
+  <image width="20" height="20" source="s2_tile_01.png"/>
  </tile>
  <tile id="1">
-  <image width="20" height="20" source="tiles/stage_two/tile_02.png"/>
+  <image width="20" height="20" source="s2_tile_02.png"/>
  </tile>
  <tile id="2">
-  <image width="20" height="20" source="tiles/stage_two/tile_03.png"/>
+  <image width="20" height="20" source="s2_tile_03.png"/>
  </tile>
  <tile id="3">
-  <image width="20" height="20" source="tiles/stage_two/tile_04.png"/>
+  <image width="20" height="20" source="s2_tile_04.png"/>
  </tile>
  <tile id="4">
-  <image width="20" height="20" source="tiles/stage_two/tile_05.png"/>
+  <image width="20" height="20" source="s2_tile_05.png"/>
  </tile>
  <tile id="5">
-  <image width="20" height="20" source="tiles/stage_two/tile_06.png"/>
+  <image width="20" height="20" source="s2_tile_06.png"/>
  </tile>
  <tile id="6">
-  <image width="20" height="20" source="tiles/stage_two/tile_07.png"/>
+  <image width="20" height="20" source="s2_tile_07.png"/>
  </tile>
  <tile id="7">
-  <image width="20" height="20" source="tiles/stage_two/tile_08.png"/>
+  <image width="20" height="20" source="s2_tile_08.png"/>
  </tile>
  <tile id="8">
-  <image width="20" height="20" source="tiles/stage_two/tile_09.png"/>
+  <image width="20" height="20" source="s2_tile_09.png"/>
  </tile>
  <tile id="9">
-  <image width="20" height="20" source="tiles/stage_two/tile_10.png"/>
+  <image width="20" height="20" source="s2_tile_10.png"/>
  </tile>
  <tile id="10">
-  <image width="20" height="20" source="tiles/stage_two/tile_11.png"/>
+  <image width="20" height="20" source="s2_tile_11.png"/>
  </tile>
  <tile id="11">
-  <image width="20" height="20" source="tiles/stage_two/tile_12.png"/>
+  <image width="20" height="20" source="s2_tile_12.png"/>
  </tile>
  <tile id="12">
-  <image width="20" height="20" source="tiles/stage_two/tile_13.png"/>
+  <image width="20" height="20" source="s2_tile_13.png"/>
  </tile>
  <tile id="13">
-  <image width="20" height="20" source="tiles/stage_two/tile_14.png"/>
+  <image width="20" height="20" source="s2_tile_14.png"/>
  </tile>
 </tileset>
